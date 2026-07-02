0d8d9583b7240de57c2bfee427f1ed17690b88ec
Dion Cherian <dioncherian@gmail.com>
2026-07-02 20:09:17 +0100

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,14 @@
+a, b, c = 2, 8, 5 
+# a = 2, b = 8, c = 5
+
+product = (a * b) // c
+reminder = (a * b) % c
+result1 = a ** b
+result2 = b ** c
+
+print(product)
+print(reminder)
+print(result1)
+print(result2)
+
+

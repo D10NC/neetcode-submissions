372857bce7752a5d4eb1155966dc35e58932be0b
Dion Cherian <dioncherian@gmail.com>
2026-07-02 20:17:15 +0100

    Add: python-boolean-or - submission-0

---
+++ Python For Beginners/python-boolean-or/submission-0.py
@@ -0,0 +1,14 @@
+a, b, c, d = False, False, True, True
+
+logic1 = a or b
+print(logic1)
+
+logic2 = b or c
+print(logic2)
+
+logic3 = c or d
+print(logic3)
+
+logic4 = a or b or c or d
+print(logic4)
+

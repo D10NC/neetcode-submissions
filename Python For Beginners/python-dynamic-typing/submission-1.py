68b1907b8547ff7723f0791187ffaf2e2e1cc02e
Dion Cherian <dioncherian@gmail.com>
2026-07-02 19:39:42 +0100

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,14 @@
+variable = 1
+print(type(variable))
+
+variable = 2.1
+print(type(variable))
+
+variable = False
+print(type(variable))
+
+variable = ""
+print(type(variable))
+
+variable = [1,2,3]
+print(type(variable))

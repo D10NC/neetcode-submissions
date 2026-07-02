60ecba24a7d796b2c89546dcafaec454fb8c2146
Dion Cherian <dioncherian@gmail.com>
2026-07-02 20:59:51 +0100

    Add: python-multiple-parameters - submission-1

---
+++ Python For Beginners/python-multiple-parameters/submission-1.py
@@ -0,0 +1,15 @@
+def two_sum(a, b):
+    sum_for_two = a + b
+    print(sum_for_two)
+
+two_sum(10, 7)
+
+def three_sum(a, b, c):
+    sum_for_three = a + b + c
+    print(sum_for_three)
+
+three_sum(6,9,1)
+
+# do not modify below this line
+two_sum(10, 9)
+three_sum(5, 14, 6)

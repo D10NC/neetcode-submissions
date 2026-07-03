116b5471fa9fd88be7576c7be8ea2084dd6630ef
Dion Cherian <dioncherian@gmail.com>
2026-07-03 17:45:00 +0100

    Add: python-if-else - submission-1

---
+++ Python For Beginners/python-if-else/submission-1.py
@@ -0,0 +1,22 @@
+def get_min(a: int, b: int) -> int:
+
+    if a > b:
+        return b
+    
+    elif a < b:
+        return a
+
+    elif a == b:
+        return a
+
+    else:
+        print("") 
+
+
+
+
+    
+# don't modify code below this line
+print(get_min(10, 11))
+print(get_min(5, -7))
+print(get_min(20, 20))
